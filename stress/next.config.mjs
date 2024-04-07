4089165dae6810cd2ad8bbfbd20e70a2abd24591
22044242-MohammadHarris <22044242@myrp.edu.sg>
2024-04-07 11:05:23 +0800

    Update - remove unnecessary files

--- stress/next.config.mjs
+++ stress/next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: "loose", // <-- add this
-    serverComponentsExternalPackages: ["mongoose"], // <-- and this
-  },
-  // and the following to enable top-level await support for Webpack
-  webpack: (config) => {
-    config.experiments = {
-      topLevelAwait: true,
-    };
-    return config;
-  },
+  // 
 };
 
 export default nextConfig;
